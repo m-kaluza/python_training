a152d9eedb876baf99921671d55f74b6c8f68700
m-kaluza <orchid@vp.pl>
2019-11-13 21:51:00 +0100

    delete unused methods

--- test_add_group.py
+++ test_add_group.py
@@ -66,20 +66,7 @@ class TestAddGroup(unittest.TestCase):
         self.return_to_groups_page(wd)
         self.logout(wd)
 
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
+   
     def tearDown(self):
         self.wd.quit()
 
