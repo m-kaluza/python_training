2282f0918adaa2623dc46adb08ce5eac4e92587f
m-kaluza <orchid@vp.pl>
2019-11-20 22:06:18 +0100

    add fixture for all tests

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-from fixture.appcontact import Appcontact
-
-@pytest.fixture
-def app(request):
-    fixture = Appcontact()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
-        app.session.login(username="admin", password="secret")
-        app.contact.create(Contact(firstname="Anna", lastname="Nowak", title="miss", mobile="692444520", email="anna@interia.pl", address="ul. Nowa 1, Katowice"))
-        app.session.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(
+        Contact(firstname="Anna", lastname="Nowak", title="miss", mobile="692444520", email="anna@interia.pl",
+                address="ul. Nowa 1, Katowice"))
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-        app.session.login(username="admin", password="secret")
-        app.contact.create(Contact(firstname="", lastname="", title="", mobile="", email="", address=""))
-        app.session.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="", lastname="", title="", mobile="", email="", address=""))
+    app.session.logout()
