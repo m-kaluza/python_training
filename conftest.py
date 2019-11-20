d8eafd7cd1a34fb0a3714f979447017a9decab09
m-kaluza <orchid@vp.pl>
2019-11-20 22:07:00 +0100

    Revert "add fixture for all tests"
    This reverts commit 2282f0918adaa2623dc46adb08ce5eac4e92587f.

--- conftest.py
+++
@@ -1,9 +0,0 @@
-from fixture.application import Application
-import pytest
-
-
-@pytest.fixture(scope="session")
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
