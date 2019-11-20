aa9be22ebf5ab1dbd6fe9eb77ccd84cd064eaedc
m-kaluza <orchid@vp.pl>
2019-11-20 20:29:20 +0100

    add class to work with Contacts

--- fixture/appcontact.py
+++ fixture/appcontact.py
@@ -1,55 +1,18 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.contact import ContactHelper
 
 class Appcontact:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        # init contact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        # submit contact creation
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy (self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,12 +11,12 @@ def app(request):
 
 
 def test_add_contact(app):
-        app.login( username="admin", password="secret")
-        app.create_contact(Contact(firstname="Anna", lastname="Nowak", title="miss", mobile="692444520", email="anna@interia.pl", address="ul. Nowa 1, Katowice"))
-        app.logout()
+        app.session.login(username="admin", password="secret")
+        app.contact.create(Contact(firstname="Anna", lastname="Nowak", title="miss", mobile="692444520", email="anna@interia.pl", address="ul. Nowa 1, Katowice"))
+        app.session.logout()
 
 
 def test_add_empty_contact(app):
-        app.login(username="admin", password="secret")
-        app.create_contact(Contact(firstname="", lastname="", title="", mobile="", email="", address=""))
-        app.logout()
+        app.session.login(username="admin", password="secret")
+        app.contact.create(Contact(firstname="", lastname="", title="", mobile="", email="", address=""))
+        app.session.logout()
