09eb4fad7f4feb50eaf669f59958059a4fb76176
m-kaluza <orchid@vp.pl>
2019-11-24 21:32:14 +0100

    modify test_edit_group

--- fixture/contact.py
+++ fixture/contact.py
@@ -62,4 +62,3 @@ class ContactHelper:
         # init contact deletion
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
-        wd.switch_to.alert.accept()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 
-def test_edit_group(app):
+def test_edit_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="Name", header="Logo", footer="Comment"))
+    app.group.edit_first_group(Group(name="New Name"))
+    app.session.logout()
+
+
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="Logo"))
     app.session.logout()
