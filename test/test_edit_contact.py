990bbb907ff4b82cb5a67f1e4d7421637266e9eb
m-kaluza <orchid@vp.pl>
2019-11-24 22:28:57 +0100

    modify fixture

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,6 +2,6 @@ from model.contact import Contact
 
 
 def test_edit_contact(app):
-        app.session.login(username="admin", password="secret")
-        app.contact.edit_first_contact(Contact(firstname="Joanna", lastname="Nowakowska", title="mrs", mobile="604153233", email="joanna@interia.pl", address="ul. Międzyblokowa 11, Katowice"))
-        app.session.logout()
+    app.contact.edit_first_contact(
+        Contact(firstname="Joanna", lastname="Nowakowska", title="mrs", mobile="604153233", email="joanna@interia.pl",
+                address="ul. Międzyblokowa 11, Katowice"))
