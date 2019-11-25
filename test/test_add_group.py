a2d5fe98d7216ed97da371d0adf6e33af7a2a261
m-kaluza <orchid@vp.pl>
2019-11-25 17:11:14 +0100

    Revert "Revert "modify fixture""
    This reverts commit 141b70d586684fccc78438ec09756d8c0dd41d1f.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-        app.session.login(username="admin", password="secret")
-        app.group.create(Group(name="data", header="grupa", footer="grupa"))
-        app.session.logout()
+    app.group.create(Group(name="data", header="grupa", footer="grupa"))
 
 
 def test_add_empty_group(app):
-        app.session.login(username="admin", password="secret")
-        app.group.create(Group(name="", header="", footer=""))
-        app.session.logout()
-
+    app.group.create(Group(name="", header="", footer=""))
