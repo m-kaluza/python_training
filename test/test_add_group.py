71ba4cfc3fdb1a2992d894d3e318aba7069764b7
m-kaluza <orchid@vp.pl>
2020-03-03 20:45:49 +0100

    sending-parameters

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,9 +10,9 @@ def random_string(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Group(name="", header="", footer="") + [
+testdata = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 5), header=random_string("header", 10), footer=random_string("footer", 15))
-    for i in range(5)
+    for i in range(1)
 ]
 
 
@@ -21,7 +21,6 @@ testdata = [Group(name="", header="", footer="") + [
     # for name in ["", random_string("name", 5)]
     # for header in ["", random_string("header", 10)]
     # for footer in ["", random_string("footer", 15)]
-]
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
