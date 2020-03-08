316ad65b4a20550f7fdc09a03ec1feb595bb8e28
m-kaluza <orchid@vp.pl>
2020-03-08 18:17:30 +0100

    loading test

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ from fixture.application import Application
 import pytest
 import json
 import os.path
+import importlib
 
 
 
@@ -39,3 +40,13 @@ def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
 
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_form_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_form_module(module):
+    return importlib.import_module("data.%s" % module).testdata
